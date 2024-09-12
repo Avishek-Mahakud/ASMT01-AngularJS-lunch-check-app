17c7e0420a75648b539ce23de3cec33b0c098da4
Avishek Mahakud <avishek.mahakud@GSLAB.COM>
2024-09-12 22:53:31 +0530

    Web files for lunch check app

---
+++ app.js
@@ -0,0 +1,34 @@
+// Define the AngularJS module
+(function() {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.lunchItems = '';
+        $scope.message = '';
+
+        $scope.checkLunch = function() {
+            var items = $scope.lunchItems.split(',').filter(function(item) {
+                return item.trim() !== '';
+            });
+
+            if ($scope.lunchItems.trim() === '') {
+                $scope.message = 'Please enter data first';
+                document.querySelector('input').style.borderColor = 'red';
+                document.querySelector('p').style.color = 'red';
+            } else if (items.length <= 3) {
+                $scope.message = 'Enjoy!';
+                document.querySelector('input').style.borderColor = 'green';
+                document.querySelector('p').style.color = 'green';
+            } else {
+                $scope.message = 'Too much!';
+                document.querySelector('input').style.borderColor = 'green';
+                document.querySelector('p').style.color = 'green';
+            }
+        };
+    }
+})();
